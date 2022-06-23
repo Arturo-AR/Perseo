63f8e5054b5fdb97d3cbacaf86df82e2f5093019
Arturo-AR <arturo.ar9703@gmail.com>
2022-06-23 17:35:15 -0500

    Se agregaron pantallas de seleccion y agenda

---
+++ app/src/main/java/com/cv/perseo/screens/ordersoptions/OrdersOptionsScreen.kt
@@ -0,0 +1,8 @@
+package com.cv.perseo.screens.ordersoptions
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun OrdersOptionsScreen() {
+
+}
---
+++ app/src/main/java/com/cv/perseo/screens/scheduleorders/ScheduleOrdersScreen.kt
@@ -0,0 +1,8 @@
+package com.cv.perseo.screens.scheduleorders
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ScheduleOrdersScreen() {
+
+}
